4641edcf25f80aca151627c1a74ffa6dde860aca
Sena <s.kilinc.ce@gmail.com>
2022-08-21 20:50:31 +0300

    Added taksimetre and configured hipotenüs

---
+++ Taksimetre/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm= 2.20,total=10;
+        Scanner input= new Scanner(System.in);
+        System.out.println("Mesafeyi km cinsinden giriniz: ");
+        km=input.nextInt();
+        total += km*perKm;
+        total = total<20? 20:total;
+        System.out.println("Verilecek ücret: "+total);
+    }
+}
