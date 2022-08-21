e8d39dca0b30655ea147855153bfcf3f4378ade9
Sena <s.kilinc.ce@gmail.com>
2022-08-21 19:47:09 +0300

    Üçgenin Alanı Projesi

---
+++ ÜçgeninÇevresi/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a,b,c,u,alan;
+        System.out.println("1.Kenarı giriniz :");
+        a= input.nextDouble();
+        System.out.println("2.Kenarı giriniz :");
+        b= input.nextDouble();
+        System.out.println("3.Kenarı giriniz :");
+        c= input.nextDouble();
+        u= (a+b+c)/2;
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Üçgenin Alanı: "+alan);
+        
+    }
+}
