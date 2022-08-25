afb98bd604efecc7ac54e7c18a886b2505714ffe
Sena <s.kilinc.ce@gmail.com>
2022-08-25 11:29:19 +0300

    added evennumssum project.

---
+++ EvenNumsSum/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int k,total=0,count=0;
+        double avg=0;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        k=inp.nextInt();
+        for (int i =0;i<=k;i++){
+            if (i%3==0 && i%4==0){
+                total += i;
+                count++;
+                System.out.println(i);
+            }
+        }
+        avg=total/count;
+        System.out.println("Average: "+avg);
+    }
+}
