5e9e53b567780c78af72ce0efa0903fd55e6ce10
Sena <s.kilinc.ce@gmail.com>
2022-08-27 18:45:05 +0300

    added harmonic series

---
+++ HarmonicSeries/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double n=3;
+        double sum=0;
+        for (double i=1;i<=n;i++){
+            sum +=(1/i);
+        }
+        System.out.println(sum);
+    }
+}
