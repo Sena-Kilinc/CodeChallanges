377b0bc0bfe6739eb3ea54fd203b89143a0fd716
Sena <s.kilinc.ce@gmail.com>
2022-09-16 15:29:22 +0300

    added multidimensional array

---
+++ MultidimensionalArray/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int [][] matrix = new int[3][4];
+        int num=1;
+        for (int i=0;i<matrix.length;i++){
+            for (int j =0;j<matrix[i].length;j++){
+                matrix[i][j]= num;
+                num++;
+            }
+        }
+        for (int i=0;i<matrix.length;i++){
+            for (int j =0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
