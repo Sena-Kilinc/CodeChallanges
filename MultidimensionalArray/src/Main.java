24a55395e6ec80f1265543aa5a7491d8bffaacc6
Sena <s.kilinc.ce@gmail.com>
2022-09-17 11:05:18 +0300

    added foreach loop

--- MultidimensionalArray/src/Main.java
+++ MultidimensionalArray/src/Main.java
@@ -8,9 +8,18 @@ public class Main {
                 num++;
             }
         }
+        /*
         for (int i=0;i<matrix.length;i++){
             for (int j =0;j<matrix[i].length;j++){
                 System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+         */
+
+        for (int[] row:matrix){
+            for (int elem : row){
+                System.out.print(elem+" ");
             }
             System.out.println();
         }
